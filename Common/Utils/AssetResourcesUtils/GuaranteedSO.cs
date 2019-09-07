245f2b1df13c4b5a7d716a7a13652b80a5458563
Caio <k10.czar@gmail.com>
2019-09-07 15:04:44 -0300

    Rename AssetResourcesUtils/Ref to GuaranteedSO

---
+++ Common/Utils/AssetResourcesUtils/GuaranteedSO.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public class GuaranteedSO<T> where T : ScriptableObject
+{
+	private T _reference;
+	readonly string _path;
+	public T Reference
+	{
+		get
+		{
+			if( _reference == null ) RequestSO( ref _reference, _path );
+			return _reference;
+		}
+	}
+
+	public GuaranteedSO( string path ) { _path = path; }
+
+	static T RequestSO( ref T field, string collectionName )
+	{
+		if( field == null ) field = Resources.Load<T>( collectionName );
+#if UNITY_EDITOR
+		if( field == null && !Application.isPlaying ) field = RequestResource( collectionName );
+#endif
+		return field;
+	}
+
+#if UNITY_EDITOR
+	const string RESOURCES_DEFAULT_PATH = "Resources/";
+	static T RequestResource( string resourcePath )
+	{
+		var t = Resources.Load<T>( resourcePath );
+		if( t != null ) return t;
+
+		var firstImport = FileAdapter.Exists( $"{Application.dataPath}/{RESOURCES_DEFAULT_PATH}{resourcePath}.asset" );
+		if( firstImport ) return null;
+
+		var create = UnityEditor.EditorUtility.DisplayDialog( "Constant not Found", $"Did not find asset at {resourcePath}, do you want to create new one?", "Sure", "No" );
+		if( !create ) return null;
+
+		t = Create( RESOURCES_DEFAULT_PATH + resourcePath );
+		Debug.LogWarning( "Created new Resource at " + RESOURCES_DEFAULT_PATH + resourcePath );
+		return t;
+	}
+
+	static T Create( string newPath, bool focus = false )
+	{
+		if( !newPath.StartsWith( "Assets/" ) ) newPath = "Assets/" + newPath;
+		var asset = UnityEditor.AssetDatabase.LoadAssetAtPath<T>( newPath );
+		if( asset != null ) return asset;
+
+		asset = ScriptableObject.CreateInstance<T>();
+
+		string assetPathAndName = UnityEditor.AssetDatabase.GenerateUniqueAssetPath( newPath + ".asset" );
+
+		Debug.Log( "Try create asset at " + assetPathAndName );
+
+		UnityEditor.AssetDatabase.CreateAsset( asset, assetPathAndName );
+
+		UnityEditor.AssetDatabase.SaveAssets();
+		UnityEditor.AssetDatabase.Refresh();
+
+		if( focus )
+		{
+			UnityEditor.EditorUtility.FocusProjectWindow();
+			UnityEditor.Selection.activeObject = asset;
+		}
+
+		return asset;
+	}
+#endif
+}
