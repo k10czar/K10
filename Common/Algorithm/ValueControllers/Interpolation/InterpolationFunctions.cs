11f548435fee98a5cc54306ad6339116f8f6f489
Caio <k10.czar@gmail.com>
2019-11-14 17:49:26 -0300

    Interpolation Optimizations on Pow2 and SCurve2

--- Common/Algorithm/ValueControllers/Interpolation/InterpolationFunctions.cs
+++ Common/Algorithm/ValueControllers/Interpolation/InterpolationFunctions.cs
@@ -35,22 +35,37 @@ public class LinearInterpolation : IInterpolationFunction
 public class PowerInterpolation : IInterpolationFunction
 {
 	//TODO: Optimize Iterpolation with fast operation
-	public static readonly IInterpolationFunction Pow2 = new PowerInterpolation( 2 );
+	public static readonly IInterpolationFunction Pow2 = Power2Interpolation.Instance;
 	public static readonly IInterpolationFunction Sqrt = new PowerInterpolation( .5f );
 	public static readonly IInterpolationFunction Pow3 = new PowerInterpolation( 3 );
 	public static readonly IInterpolationFunction Cubic = new PowerInterpolation( .3333333f );
 	private readonly float _power;
 	public PowerInterpolation( float power ) { _power = power; }
 	public float Evaluate( float normilizedDuration ) => Mathf.Pow( normilizedDuration, _power );
+
+	private class Power2Interpolation : IInterpolationFunction
+	{
+		public static readonly IInterpolationFunction Instance = new Power2Interpolation();
+		public Power2Interpolation() { }
+		public float Evaluate( float normilizedDuration ) => normilizedDuration * normilizedDuration;
+	}
 }
 
 public class SCurveInterpolation : IInterpolationFunction
 {
-	public static readonly IInterpolationFunction SCurve2 = new SCurveInterpolation( 2 );
+	public static readonly IInterpolationFunction SCurve2 = SCurve2Interpolation.Instance;
 	public static readonly IInterpolationFunction SCurve4 = new SCurveInterpolation( 4 );
 	private readonly float _power;
 	public SCurveInterpolation( float power ) { _power = power; }
 	public float Evaluate( float normilizedDuration ) => ( Mathf.Pow( normilizedDuration * 2 - 1, _power ) + 1 ) / 2;
+
+
+	private class SCurve2Interpolation : IInterpolationFunction
+	{
+		public static readonly IInterpolationFunction Instance = new SCurve2Interpolation();
+		public SCurve2Interpolation() { }
+		public float Evaluate( float normilizedDuration ) => ( ( normilizedDuration * normilizedDuration * 2 - 1 ) + 1 ) / 2;
+	}
 }
 
 public class SinInterpolation : IInterpolationFunction
