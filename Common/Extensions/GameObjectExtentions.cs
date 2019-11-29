6bd770a9d7503e24daf37731e3031ead29cb68b9
K10czar <k10.czar@gmail.com>
2019-11-29 19:20:31 -0300

    GameObjectEventsRelay

---
+++ Common/Extensions/GameObjectExtentions.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public static class GameObjectExtentions
+{
+	public interface IUnityEventsRelay
+	{
+		IEventRegister OnDestroy { get; }
+	}
+
+	public class GameObjectEventsRelay : MonoBehaviour, IUnityEventsRelay
+	{
+		private readonly EventSlot _onDestroy = new EventSlot();
+		IEventRegister IUnityEventsRelay.OnDestroy => _onDestroy;
+
+		void OnDestroy() => _onDestroy.Trigger();
+	}
+
+	public static IUnityEventsRelay EventRelay( this GameObject go )
+	{
+		GameObjectEventsRelay relay = null;
+		go.RequestSibling( ref relay );
+		return relay;
+	}
+}
--- Common/Extensions/ListExtentions.cs
+++ Common/Extensions/ListExtentions.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 public static class ListExtentions
 {
 	public static T GetClamped<T>( this IList<T> list, int id ) => list[Mathf.Min( id, list.Count - 1 )];
-	public static T GetClampedOrDefault<T>( this IList<T> list, int id ) 
+	public static T GetClampedOrDefault<T>( this IList<T> list, int id )
 	{
-		if( list.Count == 0 ) return default(T); 
+		if( list.Count == 0 ) return default( T );
 		return list[Mathf.Min( id, list.Count - 1 )];
-	} 
+	}
 }
