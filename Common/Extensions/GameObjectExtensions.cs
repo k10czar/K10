24e4bcf6378c4eb00bf9cd656d3df5175465fcde
Gabriel Leite <glimaleite@gmail.com>
2020-02-20 17:02:24 -0300

    Adding a BoolState that monitors if a game object is Active or not

--- Common/Extensions/GameObjectExtensions.cs
+++ Common/Extensions/GameObjectExtensions.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
-
 public static class GameObjectExtensions
 {
 	public interface IUnityEventsRelay
 	{
 		IEventRegister OnDestroy { get; }
+		IBoolStateObserver IsActive { get; }
 	}
-
 	public class GameObjectEventsRelay : MonoBehaviour, IUnityEventsRelay
 	{
 		private readonly EventSlot _onDestroy = new EventSlot();
+		private readonly BoolState _isActive = new BoolState();
+
 		IEventRegister IUnityEventsRelay.OnDestroy => _onDestroy;
-
+		public IEventRegister OnDestroyEvent => _onDestroy;
+		public IBoolStateObserver IsActive => _isActive;
 		void OnDestroy() => _onDestroy.Trigger();
+		void OnEnable() => _isActive.SetTrue();
+		void OnDisable() => _isActive.SetFalse();
 	}
-
 	public static IUnityEventsRelay EventRelay( this GameObject go ) => go.RequestSibling<GameObjectEventsRelay>();
 }
