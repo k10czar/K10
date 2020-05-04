9df75865e395d5d3af16f2c4b6bc5489b5056b85
k10czar <k10.czar@gmail.com>
2020-05-04 13:24:56 -0300

    Format file

--- Common/Algorithm/StateRequester.cs
+++ Common/Algorithm/StateRequester.cs
@@ -17,7 +17,7 @@ public interface IStateRequesterInteraction
 	void RemoveRequest( object obj );
 	void RequestOn( IBoolStateObserver source );
 	void IgnoreOn( IBoolStateObserver source );
-    void RequestOn( IBoolStateObserver source, System.Func<bool> validation );
+	void RequestOn( IBoolStateObserver source, System.Func<bool> validation );
 	void RequestOn( GameObject gameObject, IBoolStateObserver additionalCondition = null );
 	void IgnoreOn( GameObject gameObject, IBoolStateObserver additionalCondition = null );
 }
@@ -50,7 +50,7 @@ public class StateRequester : IStateRequester
 	public void RequestOn( GameObject gameObject, IBoolStateObserver additionalCondition = null ) { _semaphore.BlockOn( gameObject, additionalCondition ); }
 	public void IgnoreOn( GameObject gameObject, IBoolStateObserver additionalCondition = null ) { _semaphore.ReleaseOn( gameObject, additionalCondition ); }
 
-    public void RequestOn(IBoolStateObserver source, System.Func<bool> validation ) { _semaphore.BlockOn( source, validation ); }
+	public void RequestOn( IBoolStateObserver source, System.Func<bool> validation ) { _semaphore.BlockOn( source, validation ); }
 
 	void InvertedStateChange( bool free ) { _onStateChange.Trigger( !free ); }
 
