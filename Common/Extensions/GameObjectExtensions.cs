7a153bc68e4cc0599ac55a0c04908da28239974b
k10czar <k10.czar@gmail.com>
2021-05-20 18:23:36 -0300

    New IUnityEventsRelay IsAlive

--- Common/Extensions/GameObjectExtensions.cs
+++ Common/Extensions/GameObjectExtensions.cs
@@ -5,16 +5,25 @@ public static class GameObjectExtensions
 	{
 		IEventRegister OnDestroy { get; }
 		IBoolStateObserver IsActive { get; }
+		IBoolStateObserver IsAlive { get; }
 	}
 	public class GameObjectEventsRelay : MonoBehaviour, IUnityEventsRelay
 	{
 		private readonly EventSlot _onDestroy = new EventSlot();
 		private readonly BoolState _isActive = new BoolState();
+		private readonly BoolState _isAlive = new BoolState( true );
 
 		IEventRegister IUnityEventsRelay.OnDestroy => _onDestroy;
 		public IEventRegister OnDestroyEvent => _onDestroy;
 		public IBoolStateObserver IsActive => _isActive;
-		void OnDestroy() => _onDestroy.Trigger();
+		public IBoolStateObserver IsAlive => _isAlive;
+
+		void OnDestroy()
+		{
+			_onDestroy.Trigger();
+			_isAlive.SetFalse();
+		}
+
 		void OnEnable() => _isActive.SetTrue();
 		void OnDisable() => _isActive.SetFalse();
 	}
