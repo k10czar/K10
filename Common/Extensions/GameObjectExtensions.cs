da7446d919d13d62c9b49326573a06c9c89abba2
Gabriel Leite <glimaleite@gmail.com>
2021-08-02 18:05:44 -0300

    added lazy LifetimeValidator to GameObject EventRelay

--- Common/Extensions/GameObjectExtensions.cs
+++ Common/Extensions/GameObjectExtensions.cs
@@ -6,22 +6,26 @@ public static class GameObjectExtensions
 		IEventRegister OnDestroy { get; }
 		IBoolStateObserver IsActive { get; }
 		IBoolStateObserver IsAlive { get; }
+		IEventValidator LifetimeValidator { get; }
 	}
 	public class GameObjectEventsRelay : MonoBehaviour, IUnityEventsRelay
 	{
 		private readonly EventSlot _onDestroy = new EventSlot();
 		private readonly BoolState _isActive = new BoolState();
 		private readonly BoolState _isAlive = new BoolState( true );
+		private ConditionalEventsCollection _lifetimeValidator;
 
 		IEventRegister IUnityEventsRelay.OnDestroy => _onDestroy;
 		public IEventRegister OnDestroyEvent => _onDestroy;
 		public IBoolStateObserver IsActive => _isActive;
 		public IBoolStateObserver IsAlive => _isAlive;
+		public IEventValidator LifetimeValidator => _lifetimeValidator ??= new ConditionalEventsCollection();
 
 		void OnDestroy()
 		{
 			_onDestroy.Trigger();
 			_isAlive.SetFalse();
+			_lifetimeValidator?.Void();
 		}
 
 		void OnEnable() => _isActive.SetTrue();
