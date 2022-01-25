405aab52cb71cae6329a3a06954096e36da332ff
Roni Silva <tonbery@gmail.com>
2022-01-25 13:15:37 -0300

    Fixes for new movement

--- Common/Extensions/ColliderExtensions.cs
+++ Common/Extensions/ColliderExtensions.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public static class ColliderExtensions
 {
-	public static Vector3 SafeClosestPoint( this CharacterController target, Vector3 point )
+	public static Vector3 SafeClosestPoint( this CapsuleCollider target, Vector3 point )
 	{
 		target.GetCapsulePoints( out Vector3 bot, out Vector3 top, out float radius );
 		var corePoint = point.OnLineSegment( bot, top );
@@ -12,11 +12,13 @@ public static class ColliderExtensions
 		return corePoint + coreDist;
 	}
 
-	public static void GetCapsulePoints( this CharacterController target, out Vector3 bot, out Vector3 top, out float radius )
+	public static void GetCapsulePoints( this CapsuleCollider target, out Vector3 bot, out Vector3 top, out float radius )
 	{
-		var pos = target.transform.position;
+		var transform = target.transform;
+		
+		var pos = transform.position;
 		var center = pos + target.center;
-		var scl = target.transform.lossyScale;
+		var scl = transform.lossyScale;
 		radius = target.radius * Mathf.Max( scl.x, scl.z );
 		var halfHeight = Mathf.Max( target.height * scl.y * .5f - radius, 0 );
 		bot = center + Vector3.down * halfHeight;
