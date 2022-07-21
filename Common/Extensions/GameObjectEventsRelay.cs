bd6a3b76f9827a508df8dc76e438a298c2858d31
k10czar <k10.czar@gmail.com>
2022-07-21 19:24:57 -0300

    OnDestroy workaround on inactive objects

--- Common/Extensions/GameObjectEventsRelay.cs
+++ Common/Extensions/GameObjectEventsRelay.cs
@@ -20,18 +20,51 @@ public class GameObjectEventsRelay : MonoBehaviour, IUnityEventsRelay
 	public IEventRegister OnDestroyEvent => _onDestroy ?? ( _onDestroy = new EventSlot() );
 	public IBoolStateObserver IsActive => _isActive ?? ( _isActive = new BoolState( enabled ) );
 	public IBoolStateObserver IsAlive => _isAlive ?? ( _isAlive = new BoolState( !_destroyed ) );
-	public IEventValidator LifetimeValidator => _lifetimeValidator ?? ( _lifetimeValidator = new ConditionalEventsCollection() );
+	public IEventValidator LifetimeValidator => _lifetimeValidator ?? NewLifetimeValidator();
+
+	IEventValidator NewLifetimeValidator()
+	{
+		if( _destroyed ) return NullValidator.Instance;
+		// Debug.Log( $"GameObjectEventsRelay.NewLifetime( {DebugName} ) => {GetStateDebug()}" );
+		_lifetimeValidator = new ConditionalEventsCollection();
+		return _lifetimeValidator;
+	}
+
+	// string _debugName;
+	// static int _count = 0;
+	// string DebugName 
+	// {
+	// 	get
+	// 	{
+	// 		if( _debugName == null )
+	// 		{
+	// 			_debugName = _count.ToString() + ")" + this.HierarchyNameOrNull();
+	// 			_count++;
+	// 		}
+	// 		return _debugName;
+	// 	}
+	// }
+
+	// void Awake() { Debug.Log( $"GameObjectEventsRelay.Awake( {DebugName} ) => {GetStateDebug()}" ); }
+	// void Start() { Debug.Log( $"GameObjectEventsRelay.Start( {DebugName} ) => {GetStateDebug()}" ); }
 
 	void OnDestroy()
+	{
+		// Debug.Log( $"GameObjectEventsRelay.OnDestroy( {DebugName} ) => {GetStateDebug()}" );
+		Clear();
+	}
+
+	void Clear()
 	{
 		_destroyed = true;
 		_onDestroy?.Trigger();
 		_isAlive?.SetFalse();
 		_lifetimeValidator?.Void();
 
-		// _onDestroy?.Clear();
-		// _isActive?.Clear();
-		// _isAlive?.Clear();
+		_onDestroy?.Clear();
+		_isActive?.Clear();
+		_isAlive?.Clear();
+		_lifetimeValidator?.Clear();
 
 		_onDestroy = null;
 		_isAlive = null;
@@ -40,5 +73,17 @@ public class GameObjectEventsRelay : MonoBehaviour, IUnityEventsRelay
 	}
 
 	void OnEnable() => _isActive?.SetTrue();
-	void OnDisable() => _isActive?.SetFalse();
+	void OnDisable()
+	{
+		// Debug.Log( $"GameObjectEventsRelay.OnDisable( {DebugName} ) => {GetStateDebug()}" );
+		_isActive?.SetFalse();
+	}
+
+	~GameObjectEventsRelay() 
+	{
+		// Debug.Log( $"~GameObjectEventsRelay.Destructor( {DebugName} ) => {GetStateDebug()}" );
+		Clear();
+	}
+
+	// string GetStateDebug() => $"{( ( _onDestroy != null ) ? "oD" : "" )}{( ( _isActive != null ) ? "aC" : "" )}{( ( _isAlive != null ) ? "aL" : "" )}{( ( _lifetimeValidator != null ) ? "lT" : "" )}";
 }
