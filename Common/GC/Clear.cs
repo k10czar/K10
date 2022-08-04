96d2fb880ed89b43d0e21b7bfc65e5b9a41f3d1c
K10 <k10.czar@gmail.com>
2022-08-03 23:24:38 -0300

    GC helper classes

--- Common/GC/Clear.cs
+++ Common/GC/Clear.cs
@@ -1,16 +1,30 @@
+using System.Runtime.CompilerServices;
 
 
 //I dont want to mix with IDisposable features, because I cannot predict the results so I did this interface separated
+
 public static class Clear
 {
-	public static void AfterKill<T>( ref T disp ) where T : ICustomDisposableKill
+	[MethodImpl( MethodImplOptions.AggressiveInlining )]
+	public static void AfterKill<T>( ref T data ) where T : ICustomDisposableKill
 	{
-		disp?.Kill();
-		Clear.Now( ref disp );
+		data?.Kill();
+		Clear.Now( ref data );
 	}
 
-	public static void Now<T>( ref T disp )
+	[MethodImpl( MethodImplOptions.AggressiveInlining )]
+	public static void Now<T>( ref T data )
 	{
-		disp = default( T );
+		data = default( T );
 	}
+}
+
+// Another end point for scopes hiding Clear keyword
+public static class GcClear
+{
+	[MethodImpl( MethodImplOptions.AggressiveInlining )]
+	public static void AfterKill<T>( ref T data ) where T : ICustomDisposableKill => Clear.AfterKill<T>( ref data );
+
+	[MethodImpl( MethodImplOptions.AggressiveInlining )]
+	public static void Now<T>( ref T data ) => Clear.Now<T>( ref data );
 }
