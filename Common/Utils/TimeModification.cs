c6c57c34bf795f3f470433f766bc823c9b4bb280
K10 <k10.czar@gmail.com>
2022-09-20 15:10:34 -0300

    Colored log on TimeScaleChange event

--- Common/Utils/TimeModification.cs
+++ Common/Utils/TimeModification.cs
@@ -10,7 +10,14 @@ public static class TimeModification
 
 	private static void UpdateTimeScale()
 	{
-		if( !Mathf.Approximately( Time.timeScale, _timeScaleValue.CurrentReference ) ) Debug.Log( $"Change TimeScale from {Time.timeScale} to {_timeScaleValue.CurrentReference} [ {_timeScaleValue} ]" );
+		if( !Mathf.Approximately( Time.timeScale, _timeScaleValue.CurrentReference ) )
+		{
+#if UNITY_EDITOR
+			Debug.Log( $"<color=cyan>Change TimeScale</color> from <color=blue>{Time.timeScale}</color> to <color=blue>{_timeScaleValue.CurrentReference}</color> [ {_timeScaleValue} ]" );
+#else
+			Debug.Log( $"Change TimeScale from {Time.timeScale} to {_timeScaleValue.CurrentReference} [ {_timeScaleValue} ]" );
+#endif
+		}
 		Time.timeScale = _timeScaleValue.CurrentReference;
 	}
 }
