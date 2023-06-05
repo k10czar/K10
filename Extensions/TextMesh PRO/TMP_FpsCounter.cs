472b89735287488da1e4196c62281c5d132114fb
K10 <k10.czar@gmail.com>
2023-06-05 15:34:41 -0300

    Enhanced FPS counter

--- Extensions/TextMesh PRO/TMP_FpsCounter.cs
+++ Extensions/TextMesh PRO/TMP_FpsCounter.cs
@@ -5,7 +5,7 @@ public sealed class TMP_FpsCounter : MonoBehaviour
 {
     [Range(.1f,2),SerializeField] float _sampleSecond = .5f;
     [SerializeField,HideInInspector] TMPro.TMP_Text _textMesh;
-    List<float> _frame = new List<float>();
+    List<double> _frame = new List<double>();
     private float _accum = 0;
     // FPS accumulated over the interval
     private int _frames = 0;
@@ -14,9 +14,9 @@ public sealed class TMP_FpsCounter : MonoBehaviour
     // Left time for current interval
 
     //Avoid GC Collection;
-    string[] _fpsCounterStrings = new string[ 250 ];
+    string[] _fpsCounterStringsCache = new string[ 500 ];
 
-    private string _updateFPS;
+    private int _lastFpsValue = -1;
 
 #if UNITY_EDITOR
     void OnValidate()
@@ -27,35 +27,41 @@ public sealed class TMP_FpsCounter : MonoBehaviour
 
     string RequestString( int value )
     {
-        var clamped = Mathf.Clamp( value, 0, _fpsCounterStrings.Length - 1 );
+        var maxFpsLabel = _fpsCounterStringsCache.Length - 1;
+        var clamped = Mathf.Clamp( value, 0, maxFpsLabel );
 
-        if( _fpsCounterStrings[clamped] == null )
+        if( _fpsCounterStringsCache[clamped] == null )
         {
             var str = value.ToString() + " FPS";
-            if( clamped == 0 ) _fpsCounterStrings[ clamped ] = "<" + str;
-            else if( clamped == _fpsCounterStrings.Length - 1 ) _fpsCounterStrings[ clamped ] = ">" + str;
-            else _fpsCounterStrings[ clamped ] = str;
+            if( clamped == 0 ) _fpsCounterStringsCache[ clamped ] = "<" + str;
+            else if( clamped == maxFpsLabel ) _fpsCounterStringsCache[ clamped ] = ">" + str;
+            else _fpsCounterStringsCache[ clamped ] = str;
         }
 
-        return _fpsCounterStrings[ clamped ];
+        return _fpsCounterStringsCache[ clamped ];
     }
 
     void Update()
     {
-        var sampleTime = ( Time.time - _sampleSecond );
+        var sampleTime = ( Time.timeSinceLevelLoadAsDouble - _sampleSecond );
         while( _frame.Count > 0 && _frame[0] < sampleTime ) _frame.RemoveAt( 0 );
 
-        _timeleft -= Time.deltaTime;
-        _accum += Time.timeScale / Time.deltaTime;
-        ++_frames;
+        // _timeleft -= Time.deltaTime;
+        // _accum += Time.timeScale / Time.deltaTime;
+        // ++_frames;
 
-        var currTime = Time.time;
+        var currTime = Time.timeSinceLevelLoadAsDouble;
 
-        var sample = 1f;
+        var sample = 1.0;
         if( _frame.Count > 0 ) sample = currTime - _frame[0];
 
-        _textMesh.text = RequestString( Mathf.RoundToInt( ( _frame.Count + 1 ) / sample ) );
+        var fps = (int)( System.Math.Round( ( _frame.Count ) / sample ) + .01 );
+        if( _lastFpsValue != fps )
+        {
+            _textMesh.text = RequestString( fps );
+            _lastFpsValue = fps;
+        }
 
-        _frame.Add( Time.time );
+        _frame.Add( Time.timeSinceLevelLoadAsDouble );
     }
 }
