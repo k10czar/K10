8982a8401aa90c8f0f5d61ddc43f141fa7585369
K10 <k10.czar@gmail.com>
2023-06-08 02:57:57 -0300

    FPS average counter

---
+++ Extensions/TextMesh PRO/TMP_FpsAverageCounter.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class TMP_FpsAverageCounter : MonoBehaviour
+{
+    [SerializeField] TMPro.TMP_Text _textMesh;
+    private long _frames = 0;
+    private double _time = 0;
+
+    private char[] _string = new char[]{ '9', '9', '9', '9', '.', '9', ' ', 'a', 'v', 'g', };
+    int _lastRawDataShow = -1;
+    bool _canUpdateData = true;
+
+    void Start()
+    {
+        this.FindDescendent( ref _textMesh );
+        UpdateLabel( 0 );
+    }
+
+    public void Restart()
+    {
+        _canUpdateData = true;
+        Reset();
+    }
+
+    public void Reset()
+    {
+        _frames = 0;
+        _time = 0;
+    }
+
+    public void StopData()
+    {
+        _canUpdateData = false;
+    }
+
+    void UpdateLabel( double value )
+    {
+        var dataToShow = Mathf.RoundToInt( (float)( value * 10 ) );
+        if( _lastRawDataShow == dataToShow ) return;
+        _lastRawDataShow = dataToShow;
+        _string[0] = ( dataToShow > 9999 ) ? (char)( (int)'0' + ( dataToShow % 100000 ) / 10000 ) : ' ';
+        _string[1] = ( dataToShow > 999 ) ? (char)( (int)'0' + ( dataToShow % 10000 ) / 1000 ) : ' ';
+        _string[2] = ( dataToShow > 99 ) ? (char)( (int)'0' + ( dataToShow % 1000 ) / 100 ) : ' ';
+        _string[3] = (char)( (int)'0' + ( dataToShow % 100 ) / 10 );
+        _string[5] = (char)( (int)'0' + ( dataToShow % 10 ) );
+        _textMesh.text = new string( _string );
+    }
+
+    void Update()
+    {
+        if( !_canUpdateData ) return;
+        
+        _time += Time.deltaTime;
+        _frames++;
+        if( _time > 0 )
+        {
+            var avg = _frames / _time;
+            UpdateLabel( avg );
+        }
+    }
+}
