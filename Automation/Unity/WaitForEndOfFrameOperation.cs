5afa43322af6e004475eb87d1f7b3bf43eaff488
K10 <k10.czar@gmail.com>
2023-06-16 14:12:27 -0300

    Change timeScale operation

--- Automation/Unity/WaitForEndOfFrameOperation.cs
+++ Automation/Unity/WaitForEndOfFrameOperation.cs
@@ -1,10 +1,9 @@
-using System.Collections;
 
 
 namespace Unity.Automation
 {
 	public class WaitForEndOfFrameOperation : K10.Automation.Operation
 	{
-		public override IEnumerator ExecutionCoroutine() { yield return new UnityEngine.WaitForEndOfFrame(); }
+		public override System.Collections.IEnumerator ExecutionCoroutine() { yield return new UnityEngine.WaitForEndOfFrame(); }
 	}
 }
