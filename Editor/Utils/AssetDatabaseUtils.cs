de9fe66dcb47a440f5ac976b36d368abe44c56e4
K10 <k10.czar@gmail.com>
2023-09-05 19:08:08 -0300

    AssetDatabaseUtils.GetAll<SO>()

--- Editor/Utils/AssetDatabaseUtils.cs
+++ Editor/Utils/AssetDatabaseUtils.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -23,5 +24,19 @@ public static class AssetDatabaseUtils
                 last = i;
             }
         }
+    }
+    
+    public static T[] GetAll<T>( bool debug = true ) where T : ScriptableObject
+    {
+        string[] assetNames = AssetDatabase.FindAssets( $"t:{typeof(T).Name}" );
+        var collection = new T[assetNames.Length];
+        for( int i = 0; i < assetNames.Length; i++ )
+        {
+            var SOpath = AssetDatabase.GUIDToAssetPath( assetNames[i] );
+            var element = AssetDatabase.LoadAssetAtPath<T>( SOpath );
+            collection[ i ] = element;
+        }
+		if( debug ) Debug.Log( $"AssetDatabaseUtils<color=red>.</color><color=yellow>GetAll</color><color=red><</color><color=lime>{typeof(T).Name}</color><color=red>></color>:\n\t-{string.Join( ",\n\t-", collection.ToList().ConvertAll<string>( ( so ) => so.NameOrNull() ) )}" );
+        return collection;
     }
 }
