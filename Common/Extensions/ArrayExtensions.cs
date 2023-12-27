d9efab9828fe2c91a3f01caea6a193388b6d6545
K10 <k10.czar@gmail.com>
2023-12-27 03:32:22 -0300

    Null handler on Array.With()

--- Common/Extensions/ArrayExtensions.cs
+++ Common/Extensions/ArrayExtensions.cs
@@ -2,7 +2,7 @@ public static class ArrayExtensions
 {
     public static T[] With<T>( this T[] terms, T newElement )
     {
-        var len = terms.Length;
+        var len = terms?.Length ?? 0;
         var newArray = new T[len+1];
         for( int i = 0; i < len; i++ ) newArray[i] = terms[i];
         newArray[len] = newElement;
