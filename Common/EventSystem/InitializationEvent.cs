5b8d4b1d3b30c1c886b9aa438f3cb470dede6da2
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2024-01-01 19:29:37 -0300

    Create Initialization event

---
+++ Common/EventSystem/InitializationEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class InitializationEvent : ICustomDisposableKill
+{
+    private readonly EventSlot initialized = new();
+    private bool isInitialized;
+
+    public void SetReady()
+    {
+        Debug.Assert(!isInitialized, "Already initialized!");
+
+        isInitialized = true;
+        initialized.Trigger();
+        initialized.Kill();
+    }
+
+    public void CallWhenReady(Action callback)
+    {
+        if (isInitialized) callback.Invoke();
+        else initialized.Register(callback);
+    }
+    
+    public static implicit operator bool (InitializationEvent v) => v.isInitialized;
+
+    public void Kill()
+    {
+        initialized.Kill();
+    }
+}
