1c748ebef2c35f149bf87c83d0a01355d11ff020
K10 <k10.czar@gmail.com>
2024-01-22 11:16:00 -0300

    Fix build

--- Common/Utils/StoreGuid.cs
+++ Common/Utils/StoreGuid.cs
@@ -1,6 +1,7 @@
 
 public static class StoreGuid
 {
+#if UNITY_EDITOR
     public static void At( UnityEditor.SerializedProperty property )
     {
         var rootObject = property.serializedObject.targetObject;
@@ -8,6 +9,7 @@ public static class StoreGuid
         var guid = UnityEditor.AssetDatabase.AssetPathToGUID( path );
         if( property.stringValue != guid ) property.stringValue = guid;
     }
+#endif //UNITY_EDITOR
 
     public static bool At( ref string guidField, UnityEngine.Object obj )
     {
@@ -21,12 +23,14 @@ public static class StoreGuid
 
 public static class StoreFileID
 {
+#if UNITY_EDITOR
     public static void At( UnityEditor.SerializedProperty property )
     {
         var rootObject = property.serializedObject.targetObject;
         var id = rootObject.GetInstanceID();
         if( property.longValue != id ) property.intValue = id;
     }
+#endif //UNITY_EDITOR
 
     public static bool At( ref ulong idField, UnityEngine.Object obj )
     {
