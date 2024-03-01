2199dd7ffe3a911ac50b420aee35c262365bc4e1
K10 <k10.czar@gmail.com>
2024-03-01 12:01:44 -0300

    Fix compile

--- Attributes/ReadOnlyAttribute.cs
+++ Attributes/ReadOnlyAttribute.cs
@@ -24,6 +24,4 @@
  /// <seealso cref="BeginReadOnlyGroupAttribute"/>
  /// <seealso cref="ReadOnlyAttribute"/>
  public class EndReadOnlyGroupAttribute : PropertyAttribute { }
-
- public class ExtendedSerializeReferenceAttribute : PropertyAttribute { }
  
