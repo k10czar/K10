76d2cb6d3ef2c911ffc0676f90bfdcc7e0343c48
HenriqueCamillo <henriquematarazocamillo@gmail.com>
2024-03-07 10:15:32 -0300

    Added option to toggle activity with key combination

--- Common/Utils/ToggleActivityWithKey.cs
+++ Common/Utils/ToggleActivityWithKey.cs
@@ -3,12 +3,40 @@ using UnityEngine;
 public sealed class ToggleActivityWithKey : MonoBehaviour
 {
 	[SerializeField] KeyCode _key = KeyCode.F1;
+	[SerializeField] KeyCode[] _keyCombination;
 	[SerializeField] GameObject _object;
 	[SerializeField] MonoBehaviour _behaviour;
 
 	void Update()
 	{
-		if( !Input.GetKeyDown( _key ) ) return;
+		if (CheckKeyPress() || CheckKeyCombo())
+			ToggleActivity();
+	}
+
+	bool CheckKeyPress()
+	{
+		return Input.GetKeyDown( _key );
+	}
+
+	bool CheckKeyCombo()
+	{
+		if (_keyCombination == null || _keyCombination.Length == 0)
+			return false;
+
+		bool hasAtLeastOneKeyDown = false;
+		foreach (var key in _keyCombination)
+		{
+			if (key == null || !Input.GetKey(key))
+				return false;
+
+			hasAtLeastOneKeyDown |= Input.GetKeyDown(key);
+		}
+		
+		return hasAtLeastOneKeyDown;
+	}
+
+	void ToggleActivity()
+	{
 		if( _object != null ) _object.SetActive( !_object.activeSelf );
 		if( _behaviour != null ) _behaviour.enabled = !_behaviour.enabled;
 	}
