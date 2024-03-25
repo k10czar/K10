d152eb3bae9af7422e93e5604fa96afb1a8714bd
K10 <k10.czar@gmail.com>
2024-03-25 12:38:23 -0300

    Editor operation script with no need of MonoBehaviour

--- Editor/Automation/EditorOperationWindow.cs
+++ Editor/Automation/EditorOperationWindow.cs
@@ -4,9 +4,18 @@ using K10.EditorGUIExtention;
 using Automation;
 
 
+[InitializeOnLoad]
 public sealed class AutomationWindow : EditorWindow
 {
-	[MenuItem( "K10/Automation/Config" )] static void Init() { var i = Instance; }
+	private static bool _alreadyTryed = false;
+
+	[MenuItem( "K10/Automation/Config" )] static void Open() { var i = Instance; }
+
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+	static void Init()
+	{
+		_alreadyTryed = false;   
+	}
 
 	static AutomationWindow _instance;
 	public static AutomationWindow Instance
@@ -18,9 +27,70 @@ public sealed class AutomationWindow : EditorWindow
 		}
 	}
 
+    static AutomationWindow()
+    {
+        EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+    }
+
+	private static void OnPlayModeStateChanged(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.EnteredPlayMode)
+        {
+			TriggerGameStart();
+        }
+    }
+
+	private static string _automationData;
+	public static string AutomationKey => _automationData ?? ( _automationData = "Automation@" + Application.dataPath );
+
+	public static AutomationData _data;
+	public static AutomationData Data => EditorPrefsUtils.GetPersistent( ref _data, AutomationKey );
+
+	[System.Serializable]
+	public class AutomationData : ISerializationCallbackReceiver
+	{
+		[SerializeField] public OperationObject operation;
+		[SerializeField] public bool active;
+		[SerializeField] string _debugPath;
+		[SerializeField] string _guid;
+
+		public void OnAfterDeserialize()
+		{
+			_debugPath = AssetDatabase.GUIDToAssetPath( _guid );
+			operation = AssetDatabase.LoadAssetAtPath<OperationObject>( _debugPath );
+		}
+
+		public void OnBeforeSerialize()
+		{
+			_debugPath = AssetDatabase.GetAssetPath( operation );
+			_guid = AssetDatabase.AssetPathToGUID( _debugPath );
+		}
+	}
+	
+	public static void TriggerGameStart( MonoBehaviour mb = null, bool log = true )
+	{
+		if( _alreadyTryed ) return;
+		_alreadyTryed = true;
+
+		var data = Data;
+		if( data == null )
+		{
+			Debug.LogError( "Cannot start Automation operation because persistent data is null" );
+			return;
+		}
+		if( data.operation == null )
+		{
+			Debug.Log( "No Automation operation setted to start" );
+			return;
+		}
+
+		if( !data.active ) return;
+		data.operation.ExecuteOn( mb, log );
+	}
+
 	private void OnGUI()
 	{
-		var data = EditorOperationRunner.Data;
+		var data = Data;
 		var active = data.active;
 		var newActice = active;
 		EditorGUILayout.BeginHorizontal();
@@ -43,7 +113,7 @@ public sealed class AutomationWindow : EditorWindow
 		{
 			data.operation = newOp;
 			data.active = newActice;
-			EditorPrefsUtils.SaveData( data, EditorOperationRunner.AutomationKey );
+			EditorPrefsUtils.SaveData( data, AutomationKey );
 		}
 		EditorGUILayout.EndHorizontal();
 		EditorGUILayout.EndVertical();
@@ -58,26 +128,5 @@ public sealed class AutomationWindow : EditorWindow
 		// UnityEditor.EditorGUILayout.TextArea( JsonUtility.ToJson( _data ) );
 		// UnityEditor.EditorGUILayout.TextArea( EditorPrefs.GetString( AutomationKey ) );
 		if( !active ) GuiColorManager.Revert();
-	}
-
-	[System.Serializable]
-	public class AutomationData : ISerializationCallbackReceiver
-	{
-		[SerializeField] public OperationObject operation;
-		[SerializeField] public bool active;
-		[SerializeField] string _debugPath;
-		[SerializeField] string _guid;
-
-		public void OnAfterDeserialize()
-		{
-			_debugPath = AssetDatabase.GUIDToAssetPath( _guid );
-			operation = AssetDatabase.LoadAssetAtPath<OperationObject>( _debugPath );
-		}
-
-		public void OnBeforeSerialize()
-		{
-			_debugPath = AssetDatabase.GetAssetPath( operation );
-			_guid = AssetDatabase.AssetPathToGUID( _debugPath );
-		}
 	}
 }
