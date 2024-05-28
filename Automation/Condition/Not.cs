8e4dfd704a225d5a17e485e448f77dbdf5489697
K10 <k10.czar@gmail.com>
2024-05-28 13:03:01 -0300

    Trigger on register structs

--- Automation/Condition/Not.cs
+++ Automation/Condition/Not.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 public class Not : ICondition
 {
 	[ExtendedDrawer, SerializeReference] ICondition _condition;
-	public bool Check() => !_condition.Check();
+	public bool Check() => _condition == null || !_condition.Check();
 	override public string ToString() => $"!{_condition.ToStringOrNull()}";
 }
