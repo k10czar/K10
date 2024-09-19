39157f387e2bb976178a562b15371f471491a22f
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2024-09-19 03:29:35 -0300

    Fix propertyEditor not triggering save

--- Editor/SkyxEditor/PropertyEditor.cs
+++ Editor/SkyxEditor/PropertyEditor.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 namespace Skyx.SkyxEditor
 {
-    public class PropertyEditor<T> : PropertyDrawer where T : class
+    public abstract class PropertyEditor<T> : PropertyDrawer where T : class
     {
         protected T GetTarget(SerializedProperty property) => property.GetValue<T>();
         protected PropertyCollection GetProperties(SerializedProperty property, bool forceReset = false) => PropertyCollection.Get(property, forceReset);
 
         protected virtual float ExtraHeight => 0;
         protected virtual string[] ExcludeFieldsFromHeight { get; } = Array.Empty<string>();
+
+        protected abstract void Draw(Rect rect, SerializedProperty property, GUIContent label);
+
+        public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+        {
+            property.serializedObject.Update();
+
+            Draw(rect, property, label);
+
+            if (property.serializedObject.ApplyModifiedProperties())
+                GetProperties(property, true);
+        }
+
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
