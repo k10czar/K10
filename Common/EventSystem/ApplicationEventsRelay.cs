6b00a11a3308504ad69e0b7276e1f5c08ec8821c
HenriqueCamillo <henriquematarazocamillo@gmail.com>
2025-02-19 13:44:13 -0300

    Move ApplicationEventRelay bool state initialization to Awake

--- Common/EventSystem/ApplicationEventsRelay.cs
+++ Common/EventSystem/ApplicationEventsRelay.cs
@@ -3,13 +3,18 @@ using UnityEngine;
 public class ApplicationEventsRelay : MonoBehaviour
 {
     EventSlot _onQuit = new();
-    BoolState _isFocused = new(Application.isFocused);
-    BoolState _isPaused = new(!Application.isPlaying);
+    BoolState _isFocused = new();
+    BoolState _isPaused = new();
 
     public static IEventRegister OnQuit => Eternal<ApplicationEventsRelay>.Instance._onQuit;
     public static IBoolStateObserver IsFocused  => Eternal<ApplicationEventsRelay>.Instance._isFocused;
     public static IBoolStateObserver IsPaused  => Eternal<ApplicationEventsRelay>.Instance._isPaused;
 
+    private void Awake()
+    {
+        _isFocused = new BoolState(Application.isFocused);
+        _isPaused = new BoolState(!Application.isPlaying);
+    }
 
     private void OnApplicationQuit()
     {
