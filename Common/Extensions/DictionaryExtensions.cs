71eda3707d61456c22c4cd1bb7d416e553bd3104
K10 <k10.czar@gmail.com>
2025-03-15 19:23:03 -0300

    Collections pool

--- Common/Extensions/DictionaryExtensions.cs
+++ Common/Extensions/DictionaryExtensions.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using K10;
 
 public static class DictionaryExtensions
 {
@@ -8,5 +9,18 @@ public static class DictionaryExtensions
         value = new TValue();
         dictionary.Add( key, value );
         return value;
+    }
+
+    public static void AppendCount<T>( this Dictionary<T, int> dictionary, T key, int countToAdd = 1 )
+    {
+        dictionary.TryGetValue( key, out var count );
+        count += countToAdd;
+        dictionary[key] = count;
+    }
+
+    public static void ClearAndReturnToPool<T,K>( this Dictionary<T,K> collection )
+    {
+        collection.Clear();
+        ObjectPool.Return( collection );
     }
 }
