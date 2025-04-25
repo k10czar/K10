c5791574a41c64c611e962211106b88b6d81910b
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2025-04-25 17:26:09 -0300

    Allow Clearing CatalogedUniqueStock

--- Common/EventSystem/Stock.cs
+++ Common/EventSystem/Stock.cs
@@ -1,44 +1,47 @@
 using System.Collections.Generic;
 using System.Linq;
 
-public class CatalogedUniqueStock<Key,Value> : ICustomDisposableKill where Value : IObjectLifeState
+public class CatalogedUniqueStock<Key, Value> : ICustomDisposableKill where Value : IObjectLifeState
 {
-    protected readonly Dictionary< Key, Value > _dict = new Dictionary< Key, Value >();
+	protected readonly Dictionary<Key, Value> entries = new();
 
-    EventSlot _onEntriesChanged = new EventSlot();
-    public IEventRegister OnEntriesChanged => Lazy.Request( ref _onEntriesChanged );
+	private EventSlot onEntriesChanged;
+	public IEventRegister OnEntriesChanged => Lazy.Request(ref onEntriesChanged);
+
+	public void AddEntry(Key key, Value t)
+	{
+		if (entries.ContainsKey(key)) entries.Remove(key);
+		entries.Add(key, t);
+		t.IsValid.RegisterOnFalse(() => RemoveEntry(key));
+		onEntriesChanged?.Trigger();
+	}
+
+	public bool RemoveEntry(Key key)
+	{
+		var removed = entries.Remove(key);
+		if (removed) onEntriesChanged?.Trigger();
+
+		return removed;
+	}
+
+	public bool ContainsKey(Key key) => entries.ContainsKey(key);
+
+	public Value GetEntry(Key key) => entries[key];
+
+	public bool TryGetValue(Key key, out Value t) => entries.TryGetValue(key, out t);
+
+	public void Clear() => entries?.Clear();
+
+	public int Count => entries.Count;
 
 	public void Kill()
 	{
-		_dict?.Clear();
-		_onEntriesChanged?.Kill();
+		entries?.Clear();
+		onEntriesChanged?.Kill();
 	}
-
-    public void AddEntry( Key key, Value t )
-    {
-        if (_dict.ContainsKey(key)) _dict.Remove(key);
-        _dict.Add( key, t );
-        t.IsValid.RegisterOnFalse( () => RemoveEntry( key ) );
-        _onEntriesChanged?.Trigger();
-    }
-
-    public bool ContainsKey( Key key ) { return _dict.ContainsKey( key ); }
-    public Value GetEntry( Key key ) { return _dict[ key ]; }
-
-    public bool RemoveEntry(Key key)
-    {
-	    var removed = _dict.Remove(key);
-	    if (removed) _onEntriesChanged?.Trigger();
-
-	    return removed;
-    }
-
-    public bool TryGetValue( Key key, out Value t ) { return _dict.TryGetValue( key, out t ); }
-
-    public int Count => _dict.Count;
 
 	public override string ToString()
 	{
-		return $"[ {string.Join( ", ", _dict.ToList().ConvertAll( ( kvp ) => $"({kvp.Key.ToStringOrNull()} => {kvp.Value.ToStringOrNull()})" ) )} ]";
+		return $"[ {string.Join(", ", entries.ToList().ConvertAll((kvp) => $"({kvp.Key.ToStringOrNull()} => {kvp.Value.ToStringOrNull()})"))} ]";
 	}
 }
