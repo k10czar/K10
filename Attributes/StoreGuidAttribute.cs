0a725241f91169ecef805d0e41dae556a7d90c5d
K10 <k10.czar@gmail.com>
2025-05-19 12:32:47 -0300

    Attributes reorg

--- Attributes/StoreGuidAttribute.cs
+++ Attributes/StoreGuidAttribute.cs
@@ -9,9 +9,9 @@ public class StoreGuidFromAttribute : PropertyAttribute
     public readonly string NewPath = string.Empty;
     public readonly bool AllowSceneObjects = false;
 
-    public StoreGuidFromAttribute( Type typeRestriction, bool allowSceneObjects = false, string newPath = "" ) 
-    { 
-        TypeRestriction = typeRestriction; 
+    public StoreGuidFromAttribute(Type typeRestriction, bool allowSceneObjects = false, string newPath = "")
+    {
+        TypeRestriction = typeRestriction;
         AllowSceneObjects = allowSceneObjects;
         NewPath = newPath;
     }
