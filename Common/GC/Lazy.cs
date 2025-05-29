8f6de5bc2df6692202c6efb1f51b6ad230c1a0bd
K10 <k10.czar@gmail.com>
2025-05-29 17:00:04 -0300

    Lazy Poolable

--- Common/GC/Lazy.cs
+++ Common/GC/Lazy.cs
@@ -1,22 +1,36 @@
 using System.Runtime.CompilerServices;
+using K10;
 
 //I dont want to mix with IDisposable features, because I cannot predict the results so I did this interface separated
 public static class Lazy
 {
-	[MethodImpl( MethodImplOptions.AggressiveInlining )]
+	[MethodImpl(Optimizations.INLINE_IF_CAN)]
 	public static T Request<T>( ref T field ) where T : class, new()
 	{
 		return field ?? ( field = new T() );
 		// if( field == null ) field = new T();
 		// return field;
 	}
+	
+	[MethodImpl(Optimizations.INLINE_IF_CAN)]
+	public static T RequestPoolable<T>( ref T field ) where T : class, new()
+	{
+		return field ?? ( field = ObjectPool<T>.Request() );
+	}
 
-	[MethodImpl( MethodImplOptions.AggressiveInlining )]
+	[MethodImpl(Optimizations.INLINE_IF_CAN)]
 	public static T Request<T>( ref T field, bool alreadyKilled ) where T : class, new()
 	{
 		if( alreadyKilled ) return field;
 		return Request<T>( ref field );
 		// if( field == null ) field = new T();
 		// return field;
+	}
+	
+	[MethodImpl(Optimizations.INLINE_IF_CAN)]
+	public static T RequestPoolable<T>( ref T field, bool alreadyKilled ) where T : class, new()
+	{
+		if( alreadyKilled ) return field;
+		return RequestPoolable( ref field );
 	}
 }
