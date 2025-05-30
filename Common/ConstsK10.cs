f8561b775ef4d701f571d6ad54fcda00ec813f66
K10 <k10.czar@gmail.com>
2025-05-30 20:28:43 -0300

    New Code metrics keyword

--- Common/ConstsK10.cs
+++ Common/ConstsK10.cs
@@ -5,10 +5,13 @@ public static class ConstsK10
 {
 	public const string NULL_STRING = "NULL";
 
-#if UNITY_EDITOR
-	// Ever Debug on Unity Editor
-	public const string CODE_METRICS_CONDITIONAL = "UNITY_EDITOR";
+#if UNITY_EDITOR 
+#if EDITOR_WITHOUT_DEBUG 
+	public const string CODE_METRICS_CONDITIONAL = "OOOO";
 #else
-	public const string CODE_METRICS_CONDITIONAL = "CODE_METRICS";
+	public const string CODE_METRICS_CONDITIONAL = "UNITY_EDITOR";
+#endif
+#else
+	public const string CODE_METRICS_CONDITIONAL = "DEBUG";
 #endif
 }
