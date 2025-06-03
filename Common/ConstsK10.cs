2e88c447ad4fea1a0c1be42242c0888fb33880f0
K10 <k10.czar@gmail.com>
2025-06-03 17:12:11 -0300

    Revert code metrics preprocessor

--- Common/ConstsK10.cs
+++ Common/ConstsK10.cs
@@ -5,10 +5,5 @@ public static class ConstsK10
 {
 	public const string NULL_STRING = "NULL";
 
-#if UNITY_EDITOR
-	// Ever Debug on Unity Editor
-	public const string CODE_METRICS_CONDITIONAL = "UNITY_EDITOR";
-#else
 	public const string CODE_METRICS_CONDITIONAL = "CODE_METRICS";
-#endif
 }
