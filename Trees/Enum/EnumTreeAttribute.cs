55f91d3b5e2df8f34e7b15ec4a475dbe2b76ba57
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2025-06-25 08:16:41 -0300

    New EnumTree constructor

--- Trees/Enum/EnumTreeAttribute.cs
+++ Trees/Enum/EnumTreeAttribute.cs
@@ -21,8 +21,8 @@ namespace Skyx.Trees
         }
 
         public EnumTreeAttribute(bool hide) : this(null, hide: hide) {}
-        public EnumTreeAttribute(bool hide, params object[] path) : this(path, hide: hide) {}
         public EnumTreeAttribute(params object[] path) : this(path, "") {}
+        public EnumTreeAttribute(int _, string valueDisplayName) : this(null, valueDisplayName, valueDisplayName) {}
 
         public EnumTreeAttribute(string[] path, string valueDisplayName = "") : this(path.ToArray<object>(), valueDisplayName) {}
     }
