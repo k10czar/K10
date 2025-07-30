4519191f375260c309f872f5a2de64630fc41ecb
K10 <k10.czar@gmail.com>
2025-07-30 20:20:27 -0300

    EnumCache names

--- Optimizations/EnumCache.cs
+++ Optimizations/EnumCache.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 public static class EnumCache<T> where T : Enum, IConvertible
 {
     static IReadOnlyList<T> _cachedValues = null;
+    static IReadOnlyList<string> _cachedNames = null;
     static int _count = -1;
 
     public static IReadOnlyList<T> GetValues()
@@ -16,6 +17,29 @@ public static class EnumCache<T> where T : Enum, IConvertible
             _cachedValues = list;
         }
         return _cachedValues;
+    }
+
+    public static IReadOnlyList<string> GetNames()
+    {
+        if (_cachedValues == null)
+        {
+            var reference = Enum.GetValues(typeof(T));
+            var list = new List<T>(reference.Length);
+            foreach (var e in reference) list.Add((T)e);
+            _cachedValues = list;
+        }
+        if (_cachedNames == null)
+        {
+            var names = new List<string>();
+            foreach (var e in _cachedValues) names.Add( e.ToString() );
+            _cachedNames = names;
+        }
+        return _cachedNames;
+    }
+
+    public static string GetCachedNameFromId( int id )
+    {
+        return GetNames()[id];
     }
 
     public static int GetCount()
