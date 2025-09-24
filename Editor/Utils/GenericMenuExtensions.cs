0b47341ff061fc83a274339780ce41aa2584b308
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2025-09-24 13:53:29 -0300

    Generic Menu Extensions

---
+++ Editor/Utils/GenericMenuExtensions.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace K10.EditorUtils
+{
+    public static class GenericMenuExtensions
+    {
+        public static void AddItem(this GenericMenu menu, string label, GenericMenu.MenuFunction action)
+            => menu.AddItem(new GUIContent(label), false, action);
+    }
+}
