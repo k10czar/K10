f3b7c38eb57f0d318d2d1e34064729caa239aac9
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2025-12-12 19:13:10 -0300

    WaitForEventSlot with validator

--- Common/EventSystem/WaitForEventSlot.cs
+++ Common/EventSystem/WaitForEventSlot.cs
@@ -1,14 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class WaitForEventSlot : CustomYieldInstruction
 {
     private readonly IEvent targetEvent;
+    private readonly Func<bool> validator;
+
     private bool eventTriggered = false;
 
     public override bool keepWaiting => !eventTriggered;
 
     private void Triggered()
     {
+        if (validator != null && !validator.Invoke())
+            return;
+
         eventTriggered = true;
         targetEvent.Unregister(Triggered);
     }
@@ -16,6 +22,14 @@ public class WaitForEventSlot : CustomYieldInstruction
     public WaitForEventSlot(IEvent targetEvent)
     {
         this.targetEvent = targetEvent;
+        targetEvent.Register(Triggered);
+    }
+
+    public WaitForEventSlot(IEvent targetEvent, Func<bool> validator)
+    {
+        this.targetEvent = targetEvent;
+        this.validator = validator;
+
         targetEvent.Register(Triggered);
     }
 }
