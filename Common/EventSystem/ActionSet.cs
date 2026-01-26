5e3edb2a263d380b09c1d92f73ac079f8ed6a36e
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2026-01-26 01:27:03 -0300

    Allow voiding single event in ActionSet

--- Common/EventSystem/ActionSet.cs
+++ Common/EventSystem/ActionSet.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using K10;
+using UnityEngine;
 
 public class ActionSet
 {
@@ -9,31 +10,44 @@ public class ActionSet
 
     #region Register Interface
 
-    public void Register(IEventRegister target, Action action)
+    public IVoidable Register(IEventRegister target, Action action)
     {
         var capsule = new ActionCapsule(action, target);
         registeredActions.Add(capsule);
+        return capsule;
     }
 
-    public void Register<T>(IEventRegister<T> target, Action<T> action)
+    public IVoidable Register<T>(IEventRegister<T> target, Action<T> action)
     {
         var capsule = new ActionCapsule<T>(action, target);
         registeredActions.Add(capsule);
+        return capsule;
     }
 
-    public void Register<T, K>(IEventRegister<T, K> target, Action<T, K> action)
+    public IVoidable Register<T, K>(IEventRegister<T, K> target, Action<T, K> action)
     {
         var capsule = new ActionCapsule<T, K>(action, target);
         registeredActions.Add(capsule);
+        return capsule;
     }
 
-    public void Register<T, K, L>(IEventRegister<T, K, L> target, Action<T, K, L> action)
+    public IVoidable Register<T, K, L>(IEventRegister<T, K, L> target, Action<T, K, L> action)
     {
         var capsule = new ActionCapsule<T, K, L>(action, target);
         registeredActions.Add(capsule);
+        return capsule;
     }
 
     #endregion
+
+    #region Void Interface
+
+    public void Void(IVoidable voidable)
+    {
+        var removed = registeredActions.Remove(voidable);
+        Debug.Assert(removed, "Trying to void non tracked IVoidable!");
+        voidable.Void();
+    }
 
     public void Void()
     {
@@ -43,6 +57,8 @@ public class ActionSet
         foreach (var capsule in entries)
             capsule.Void();
     }
+
+    #endregion
 
     ~ActionSet() => ObjectPool<List<IVoidable>>.Return(registeredActions);
 }
