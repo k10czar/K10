713dc95dc2487a0f7667551c611161babba264aa
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2026-02-11 19:06:34 -0300

    Explose ClampedFloatState min/max

--- Common/Algorithm/ValueStates/NumberState/ClampedFloatState.cs
+++ Common/Algorithm/ValueStates/NumberState/ClampedFloatState.cs
@@ -3,22 +3,24 @@
 [System.Serializable]
 public class ClampedFloatState : FloatState
 {
-    [SerializeField] private float minValue = float.MinValue;
-    [SerializeField] private float maxValue = float.MaxValue;
+    [field: SerializeField] public float MinValue { get; private set; }
+    [field: SerializeField] public float MaxValue { get; private set; }
+
+    public float Percent => MaxValue - MinValue != 0 ? (Value - MinValue) / (MaxValue - MinValue) : 0;
 
     public override void Setter(float newValue)
     {
-        newValue = Mathf.Clamp(newValue, minValue, maxValue);
+        newValue = Mathf.Clamp(newValue, MinValue, MaxValue);
         base.Setter(newValue);
     }
 
     public void ChangeBounds(float min, float max)
     {
-        minValue = min;
-        maxValue = max;
+        MinValue = min;
+        MaxValue = max;
 
         Setter(Value);
     }
 
-    public ClampedFloatState(int initialValue = default) : base(initialValue) {}
+    public ClampedFloatState(int initialValue = 0) : base(initialValue) {}
 }
