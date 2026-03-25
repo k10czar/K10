e7e70053f69c048f78d0c4b93470505454659f0a
Rafael Martins <ra.martins13@gmail.com>
2026-03-25 15:33:18 -0300

    Add missing xbox support

--- Porting/Xbox/GdkPlatformSettingsData.cs
+++ Porting/Xbox/GdkPlatformSettingsData.cs
@@ -1,12 +1,11 @@
-#if MICROSOFT_GDK_SUPPORT || UNITY_GAMECORE
 using UnityEngine;
 
 public class GdkPlatformSettingsData : ScriptableObject, IGdkRuntimeData
 {
-    [SerializeField] string titleId = "FFFFFFFF";
-    [SerializeField] string scid = "00000000-0000-0000-0000-0000FFFFFFFF";
-    [SerializeField] string saveScid = "00000000-0000-0000-0000-0000FFFFFFFF";
-    [SerializeField] string sandbox = "";
+    [SerializeField] string titleId = "75B4A042";
+    [SerializeField] string scid = "00000000-0000-0000-0000-000075b4a042";
+    [SerializeField] string saveScid = "00000000-0000-0000-0000-000075b4a042";
+    [SerializeField] string sandbox = "ZKLCLN.2";
 
     public string Sandbox => sandbox; 
     public string Scid => scid; 
@@ -14,4 +13,3 @@ public class GdkPlatformSettingsData : ScriptableObject, IGdkRuntimeData
     public string TitleId => titleId;
     public uint TitleIdNumeric => uint.Parse(titleId, System.Globalization.NumberStyles.HexNumber);
 }
-#endif
