f6dbd6e823d05e915968d452769021680eb3a2e0
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2026-04-27 16:29:38 -0300

    More Unity Destroy Extensions

--- Common/Extensions/UnityDestroyExtensions.cs
+++ Common/Extensions/UnityDestroyExtensions.cs
@@ -1,72 +1,98 @@
 using UnityEngine;
 using System.Collections.Generic;
+using K10.Common;
 
-public static class UnityDestroyExtensions
+namespace K10.Common
 {
-    public static void DestroyAll( this IEnumerable<GameObject> gameObjects )
+    public static class UnityDestroyExtensions
     {
-        if( gameObjects == null ) return;
-        foreach( GameObject obj in gameObjects ) 
+        public static void SafeDestroySelf(this GameObject obj)
         {
-            if( obj == null ) continue;
+            if (obj.IsNullOrDestroyed()) return;
 
-#if UNITY_EDITOR
-            if( !Application.isPlaying )
-                GameObject.DestroyImmediate( obj );
+            #if UNITY_EDITOR
+            if (!Application.isPlaying) Object.DestroyImmediate(obj);
             else
-#endif
-                GameObject.Destroy( obj );
+            #endif
+                Object.Destroy(obj);
         }
-    }
 
-    public static void DestroyAllAndClear( this IList<GameObject> gameObjects )
-    {
-        if( gameObjects == null ) return;
-        gameObjects.DestroyAll();
-        gameObjects.Clear();
-    }
-
-    public static void DestroyAll<T>( this IEnumerable<T> components ) where T : Component
-    {
-        if( components == null ) return;
-        foreach( Component obj in components ) 
+        public static void SafeDestroyGO(this Component component)
         {
-            if( obj == null ) continue;
+            if (component.IsNullOrDestroyed()) return;
 
-#if UNITY_EDITOR
-            if( !Application.isPlaying ) Component.DestroyImmediate( obj );
+            #if UNITY_EDITOR
+            if (!Application.isPlaying) Object.DestroyImmediate(component.gameObject);
             else
-#endif
-                Component.Destroy( obj );
+            #endif
+                Object.Destroy(component.gameObject);
         }
-    }
 
-    public static void DestroyAllGameObjects<T>( this IEnumerable<T> components ) where T : Component
-    {
-        if( components == null ) return;
-        foreach( Component component in components ) 
+        public static void DestroyAll(this IEnumerable<GameObject> gameObjects)
         {
-            if( component == null ) continue;
+            if (gameObjects == null) return;
+            foreach (GameObject obj in gameObjects)
+            {
+                if (obj == null) continue;
 
-#if UNITY_EDITOR
-            if( !Application.isPlaying ) GameObject.DestroyImmediate( component.gameObject );
-            else
-#endif
-                GameObject.Destroy( component.gameObject );
+                #if UNITY_EDITOR
+                if (!Application.isPlaying)
+                    GameObject.DestroyImmediate(obj);
+                else
+                #endif
+                    GameObject.Destroy(obj);
+            }
         }
-    }
 
-    public static void DestroyAllGameObjectsAndClear<T>( this IList<T> components ) where T : Component
-    {
-        if( components == null ) return;
-        components.DestroyAllGameObjects();
-        components.Clear();
-    }
+        public static void DestroyAllAndClear(this IList<GameObject> gameObjects)
+        {
+            if (gameObjects == null) return;
+            gameObjects.DestroyAll();
+            gameObjects.Clear();
+        }
 
-    public static void DestroyAllAndClear<T>( this IList<T> components ) where T : Component
-    {
-        if( components == null ) return;
-        components.DestroyAll();
-        components.Clear();
+        public static void DestroyAll<T>(this IEnumerable<T> components) where T : Component
+        {
+            if (components == null) return;
+            foreach (Component obj in components)
+            {
+                if (obj == null) continue;
+
+                #if UNITY_EDITOR
+                if (!Application.isPlaying) Component.DestroyImmediate(obj);
+                else
+                #endif
+                    Component.Destroy(obj);
+            }
+        }
+
+        public static void DestroyAllGameObjects<T>(this IEnumerable<T> components) where T : Component
+        {
+            if (components == null) return;
+            foreach (Component component in components)
+            {
+                if (component == null) continue;
+
+                #if UNITY_EDITOR
+                if (!Application.isPlaying) GameObject.DestroyImmediate(component.gameObject);
+                else
+                #endif
+                    GameObject.Destroy(component.gameObject);
+            }
+        }
+
+        public static void DestroyAllGameObjectsAndClear<T>(this IList<T> components) where T : Component
+        {
+            if (components == null) return;
+            components.DestroyAllGameObjects();
+            components.Clear();
+        }
+
+        public static void DestroyAllAndClear<T>(this IList<T> components) where T : Component
+        {
+            if (components == null) return;
+            components.DestroyAll();
+            components.Clear();
+        }
     }
 }
