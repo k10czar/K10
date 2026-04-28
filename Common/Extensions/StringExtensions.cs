6b05dbf4c382bb0051c6c2ab6815950447cf1975
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2026-04-28 17:34:16 -0300

    Move stuff around to enable AssemblyDefinitions

---
+++ Common/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rogue.REditor
+{
+    public static class StringExtensions
+    {
+        public static string Clean(this string source) => Regex.Replace(source, "[^a-zA-Z0-9]", "");
+
+        public static int LineCount(this string source) => string.IsNullOrEmpty(source) ? 0 : source.Count(entry => entry == '\n') + 1;
+
+        #if UNITY_EDITOR
+        public static string Pretty(this Enum value) => UnityEditor.ObjectNames.NicifyVariableName(value.ToString());
+        public static string Pretty(this string value) => UnityEditor.ObjectNames.NicifyVariableName(value);
+        #endif
+    }
+}
--- Editor/SkyxEditor/StringExtensions.cs
+++ Editor/SkyxEditor/StringExtensions.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Linq;
-using System.Text.RegularExpressions;
 using Skyx.RuntimeEditor;
-using UnityEditor;
 
 namespace Rogue.REditor
 {
@@ -31,12 +28,5 @@ namespace Rogue.REditor
 
             return $"{baseString} | <color={targetColor.ToHexRGB()}><size={targetSize}>{infoString}</size></color>";
         }
-
-        public static string Pretty(this Enum value) => ObjectNames.NicifyVariableName(value.ToString());
-        public static string Pretty(this string value) => ObjectNames.NicifyVariableName(value);
-
-        public static string Clean(this string source) => Regex.Replace(source, "[^a-zA-Z0-9]", "");
-
-        public static int LineCount(this string source) => string.IsNullOrEmpty(source) ? 0 : source.Count(entry => entry == '\n') + 1;
     }
 }
