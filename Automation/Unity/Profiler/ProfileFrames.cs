8e9c006af3824bc317749a6c905971d4c08a300d
K10czar <k10.czar@gmail.com>
2026-06-03 13:09:50 -0300

    Profile frames clean toogle

--- Automation/Unity/Profiler/ProfileFrames.cs
+++ Automation/Unity/Profiler/ProfileFrames.cs
@@ -12,8 +12,10 @@ namespace K10.Automation
     {
 #if UNITY_2023_1_OR_NEWER
 		[ExtendedDrawer, SerializeReference] IValueProvider<int> _framesRef;
+		[ExtendedDrawer, SerializeReference] IValueProvider<bool> _cleanRef;
 #else
 		[SerializeField] int _framesVal = 10;
+		[SerializeField] bool _cleanVal = false;
 #endif
 
 		public int Frames =>	
@@ -23,11 +25,18 @@ namespace K10.Automation
 			_framesVal;
 #endif
 
+		public bool Clean =>	
+#if UNITY_2023_1_OR_NEWER
+			_cleanRef?.Value ?? false;
+#else
+			_cleanVal;
+#endif
+
 		public override string EmojiIcon => "📊";
 
 		public override IEnumerator ExecutionCoroutine( bool log = false ) 
 		{ 
-			yield return ProfilerUtils.CaptureFramesCoroutine( Frames, log );
+			yield return ProfilerUtils.CaptureFramesCoroutine( Frames, Clean, log );
 		}
 
 		public override string ToString() => $"{base.ToString()} for {Frames}";
