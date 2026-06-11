2feb3ee05dd21ed79fad525b96de9180f19caefa
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2026-06-10 23:54:26 -0300

    ClassTreeTypePickerAttribute

--- Editor/SkyxEditor/AttributesPropertyDrawers.cs
+++ Editor/SkyxEditor/AttributesPropertyDrawers.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using Rogue.RuntimeEditor;
 using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
@@ -244,6 +246,29 @@ namespace Rogue.REditor
                 property.boolValue = !property.boolValue;
                 property.Apply();
             }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) => SkyxStyles.LineHeight;
+    }
+
+    [CustomPropertyDrawer(typeof(ClassTreeTypePickerAttribute))]
+    public class ClassTreeTypePickerAttributePropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+        {
+            var classAtt = (ClassTreeTypePickerAttribute) attribute;
+
+            SkyxGUI.DrawLabel(ref rect, label.text);
+
+            var currentType = Type.GetType(property.stringValue);
+            if (EditorGUI.DropdownButton(rect, new GUIContent(currentType?.Name ?? "NULL"), FocusType.Passive))
+                ClassTreePicker.Draw(rect, classAtt.targetType, currentType, type => OnTypeSelected(property, type));
+        }
+
+        private static void OnTypeSelected(SerializedProperty valueProp, Type selectedType)
+        {
+            valueProp.stringValue = selectedType.AssemblyQualifiedName;
+            valueProp.Apply();
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label) => SkyxStyles.LineHeight;
