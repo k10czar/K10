6d7cc68b70f49bf963584aa6aa210bbc3c386a05
Rafael Martins <ra.martins13@gmail.com>
2026-06-17 00:44:44 -0300

    Add generic ach map support

--- Porting/Xbox/GdkAchievementAdapter.cs
+++ Porting/Xbox/GdkAchievementAdapter.cs
@@ -1,5 +1,4 @@
 #if MICROSOFT_GDK_SUPPORT || UNITY_GAMECORE
-using System.Collections.Generic;
 using Unity.XGamingRuntime;
 using UnityEngine;
 using XBL = Unity.XGamingRuntime.SDK.XBL;
@@ -7,43 +6,16 @@ using XBL = Unity.XGamingRuntime.SDK.XBL;
 public static class GdkAchievementsAdapter
 {
     private const int Progress100 = 100;
+    private const string DefaultMapResourceName = "GdkAchievementMap";
 
-    public static readonly Dictionary<string, string> Map = new()
+    private static GdkAchievementMapAsset _mapAsset;
+    private static bool _missingMapWarningShown;
+
+    public static void SetMap(GdkAchievementMapAsset map)
     {
-        { "ACH_OUR_LEGACY", "15" },
-        { "ACH_OLD_COMPANIONS", "2" },
-        { "ACH_KILLED_MERCHANT", "1" },
-        { "ACH_KILLED_MATRIARCH", "4" },
-        { "ACH_KILLED_HEADCUTTER", "5" },
-        { "ACH_KILLED_TAMARINDO", "6" },
-        { "ACH_BACK_HOME", "7" },
-        { "ACH_KILLED_VULTURE", "8" },
-        { "ACH_DIVINE_HELP", "9" },
-        { "ACH_KILLED_MINISTER", "10" },
-        { "ACH_FINISH_SUBWAY_SURVIVAL", "11" },
-        { "ACH_ENTER_THE_TRAIN", "12" },
-        { "ACH_KILLED_MACHINIST", "13" },
-        { "ACH_KILLED_DRNINA", "14" },
-        { "ACH_FINAL_COUNSELOR", "15" },
-        { "ACH_RED_PORTAL", "17" },
-        { "ACH_TALK_TO_GHOSTS", "18" },
-        { "ACH_FAITH_CONSTELLATION", "19" },
-        { "ACH_DISCIPLINE_CONSTELLATION", "20" },
-        { "ACH_FURY_CONSTELLATION", "21" },
-        { "ACH_KILLED_LOOTGOBLIN", "22" },
-        { "ACH_KILLED_MERCHANT_FIRST_RUN", "23" },
-        { "ACH_KILLED_HEADCUTTER_LESS_THAN_SEVEN_RUNS", "27" },
-        { "ACH_KILLED_MINISTER_LESS_THAN_FOURTEEN_RUNS", "28" },
-        { "ACH_KILLED_NINA_LESS_THAN_TWENTYONE_RUNS", "29" },
-        { "ACH_ALL_BLESSINGS", "24" },
-        { "ACH_BLESSING_FOUNDATION", "25" },
-        { "ACH_BLESSING_OF_THE_FUTURE", "26" },
-        { "ACH_DEAL_30000_DAMAGE_SINGLEHIT", "30" },
-        { "ACH_DEAL_100000_DAMAGE_SINGLEHIT", "31" },
-        { "ACH_DEAL_7777777_DAMAGE_SINGLEHIT", "32" },
-        { "ACH_DEAL_77777777_DAMAGE_SINGLEHIT", "33" },
-        { "ACH_DEAL_777777777_DAMAGE_SINGLEHIT", "34" },
-    };
+        _mapAsset = map;
+        _missingMapWarningShown = false;
+    }
 
     private static bool TryGetGdk(out GdkGameRuntimeService gdk)
     {
@@ -66,8 +38,37 @@ public static class GdkAchievementsAdapter
         return true;
     }
 
+    private static GdkAchievementMapAsset GetMap()
+    {
+        if (_mapAsset != null)
+            return _mapAsset;
+
+        // Optional fallback: place the asset at Assets/Resources/GdkAchievementMap.asset
+        _mapAsset = Resources.Load<GdkAchievementMapAsset>(DefaultMapResourceName);
+
+        if (_mapAsset == null && !_missingMapWarningShown)
+        {
+            _missingMapWarningShown = true;
+            Debug.LogWarning(
+                "[GDK][ACH] GdkAchievementMapAsset is not assigned. " +
+                "Call GdkAchievementsAdapter.SetMap(map) during platform initialization, " +
+                "or place GdkAchievementMap.asset inside a Resources folder. " +
+                "Raw ids will still work."
+            );
+        }
+
+        return _mapAsset;
+    }
+
     private static string ResolveAchievementId(string gameCodeOrXboxId)
-        => Map.TryGetValue(gameCodeOrXboxId, out var xboxId) ? xboxId : gameCodeOrXboxId;
+    {
+        var map = GetMap();
+
+        if (map != null && map.TryResolve(gameCodeOrXboxId, out var xboxId))
+            return xboxId;
+
+        return gameCodeOrXboxId;
+    }
 
     // "Unlock" by setting 100%
     public static void Unlock(string gameCodeOrXboxId)
@@ -135,7 +136,7 @@ public static class GdkAchievementsAdapter
         XBL.XblAchievementsGetAchievementAsync(
             gdk.UserData.contextHandle,
             gdk.UserData.userXUID,
-            gdk.Scid,                
+            gdk.Scid,
             achievementId,
             (hr, result) =>
             {
