4402078fdd433d4274683cb40986f3b75e59381e
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2026-07-06 21:21:18 -0300

    InstantiateLinked

--- Common/Extensions/ObjectExtensions.cs
+++ Common/Extensions/ObjectExtensions.cs
@@ -12,5 +12,15 @@ namespace K10.Common
 
         public static string SafeName(this Object obj) => IsNullOrDestroyed(obj) ? "Dead Obj" : obj.name;
         public static string SafeToString(this Object obj) => IsNullOrDestroyed(obj) ? "NULL" : obj.ToString();
+
+        public static T InstantiateLinked<T>(this T prefab) where T : MonoBehaviour
+        {
+            #if UNITY_EDITOR
+            if (!Application.isPlaying)
+                return (T) UnityEditor.PrefabUtility.InstantiatePrefab(prefab);
+            #endif
+
+            return Object.Instantiate(prefab);
+        }
     }
 }
