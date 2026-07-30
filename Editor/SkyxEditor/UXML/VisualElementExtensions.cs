790a95e2c75a3adecabde6bd193469ecc2dfd6b7
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2026-07-30 13:52:37 -0300

    VisualElementExtensions

---
+++ Editor/SkyxEditor/UXML/VisualElementExtensions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.UIElements;
+
+namespace Rogue.REditor
+{
+    public static class VisualElementExtensions
+    {
+        public static void SetVisible(this VisualElement element, bool isVisible)
+            => element.style.display = isVisible ? DisplayStyle.Flex : DisplayStyle.None;
+    }
+}
